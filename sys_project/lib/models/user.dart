cdb7dd1ba3f056f50194ce6255bda7aea8a38e79
wassim <unlessbbx03@gmail.com>
2024-05-19 14:50:46 +0200

    user model

---
+++ sys_project/lib/models/user.dart
@@ -0,0 +1,20 @@
+class User {
+  final String name;
+  final String email;
+  final String password;
+  final String phoneNumber;
+  final String address;
+  final String profilePicture;
+  final String bio;
+  final String id;
+  User({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    required this.address,
+    required this.profilePicture,
+    required this.bio,
+    required this.id,
+  });
+}
